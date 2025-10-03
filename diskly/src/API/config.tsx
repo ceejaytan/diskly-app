ba07e901afad2124434bdb16f5018767ee3683f9
ceejaytan <tanchashmere@gmail.com>
2025-10-03 17:48:38 +0800

    Save local changes to config.tsx

--- diskly/src/API/config.tsx
+++ diskly/src/API/config.tsx
@@ -1,27 +1,25 @@
-export const API_URL = "http://192.168.1.7:8080"
+export const API_URL = "http://localhost:8080";
 
-export async function checkBackendStatus(){
-  try{
-    const res = await fetch(`${API_URL}`)
-    if (res.ok){
-      return true
-
-    }else{
-      return false
+export async function checkBackendStatus() {
+  try {
+    const res = await fetch(`${API_URL}`);
+    if (res.ok) {
+      return true;
+    } else {
+      return false;
     }
-  }catch(err){
-    return false
+  } catch (err) {
+    return false;
   }
 }
 
-export function logout_session(){
+export function logout_session() {
   fetch(`${API_URL}/accounts/logout`, {
     method: "GET",
-    credentials: "include"
-  })
-  .then( res => {
-    if (res.ok){
-        window.location.href = "/"
-      }
-    })
+    credentials: "include",
+  }).then((res) => {
+    if (res.ok) {
+      window.location.href = "/";
+    }
+  });
 }
